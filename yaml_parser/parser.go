7bd95faf3e84e997457904591ea4bd6bd38231ab
Misnaged <pa100ni@gmail.com>
2023-08-30 02:26:36 +0200

    All REST methods provided within swagger config are now collected into `[]map[string]any`...

--- yaml_parser/parser.go
+++ yaml_parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"gopkg.in/yaml.v3"
 	"warp_swagger/config_swagger"
+	"warp_swagger/utils"
 )
 
 type Parser struct {
@@ -24,27 +25,56 @@ func NewParser(path string) (*Parser, error) {
 
 var ErrNilMap = errors.New("path map is nil")
 
-func (p *Parser) CollectRESTmethods() error {
+// countRESTmethods needed to sum all REST methods
+// being provided within swagger config and return the sum number
+// Basically, needed only for CollectRESTmethods
+// where we need to set the length of []map[string]any
+func (p *Parser) countRESTmethods() (int, error) {
+	var count int
 	m := p.SwaggerCfg.SwagBP.PathsMap
-	//pm := m[title].([]*yaml.Node)
-	//if pm == nil {
-	//	return ErrNilMap
-	//}
 	for title := range m {
 		pm := m[title].([]*yaml.Node)
 		if pm == nil {
-			return ErrNilMap
+			return 0, ErrNilMap
 		}
 		for i := range pm {
 			switch pm[i].Value {
 			case "post":
-				fmt.Println("post detected")
+				count += 1
 			case "get":
-				fmt.Println("get detected")
+				count += 1
+			}
+		}
+	}
+	return count, nil
+}
 
+// CollectRESTmethods searching any (post and get, at this time) kind of REST methods
+// in the 'paths' section of config (where they normally should be defined)
+func (p *Parser) CollectRESTmethods() ([]map[string]any, error) {
+	m := p.SwaggerCfg.SwagBP.PathsMap
+	count, err := p.countRESTmethods()
+	if err != nil {
+		return nil, fmt.Errorf("error while counting rest methods: %w", err)
+	}
+	restMap := make([]map[string]any, count)
+
+	restMap = append(restMap[count:]) // cut empty maps
+
+	for title := range m {
+		pm := m[title].([]*yaml.Node)
+		if pm == nil {
+			return nil, ErrNilMap
+		}
+		for i := range pm {
+			switch pm[i].Value {
+			case "post":
+				restMap = append(restMap, utils.Unwrap(pm))
+			case "get":
+				restMap = append(restMap, utils.Unwrap(pm))
 			}
 		}
 
 	}
-	return nil
+	return restMap, nil
 }
